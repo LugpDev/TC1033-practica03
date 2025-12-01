1e380358673a600f7e36e4c06a5e1532c2a0bafb
LugpDev2022 <LugpDev2022@gmail.com>
2025-12-01 13:32:16 -0600

    feat: implement Videojuego class with constructors and getters

---
+++ Videojuego.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include "Videojuego.h"
+
+using namespace std;
+
+Videojuego::Videojuego()
+{
+  titulo = "";
+  genero = "";
+  anioDeLanzamiento = 0;
+}
+
+Videojuego::Videojuego(string _titulo, string _genero, int _anioDeLanzamieto)
+{
+  titulo = _titulo;
+  genero = _genero;
+  anioDeLanzamiento = _anioDeLanzamieto;
+}
+
+string Videojuego::getTitulo()
+{
+  return titulo;
+}
+
+string Videojuego::getGenero()
+{
+  return genero;
+}
+
+int Videojuego::getAnioDeLanzamiento()
+{
+  return anioDeLanzamiento;
+}
+
+void Videojuego::setTitulo(string nuevoTitulo)
+{
+  titulo = nuevoTitulo;
+}
+
+void Videojuego::setGenero(string nuevoGenero)
+{
+  genero = nuevoGenero;
+}
+
+void Videojuego::setAnioDeLanzamiento(int nuevoAnio)
+{
+  anioDeLanzamiento = nuevoAnio;
+}
---
+++ Videojuego.h
@@ -0,0 +1,28 @@
+#ifndef __VIDEOJUEGO_H__
+#define __VIDEOJUEGO_H__
+
+#include <string>
+
+using namespace std;
+
+class Videojuego
+{
+private:
+  string titulo;
+  string genero;
+  int anioDeLanzamiento;
+
+public:
+  Videojuego();
+  Videojuego(string _titulo, string _genero, int _anioDeLanzamieto);
+
+  string getTitulo();
+  string getGenero();
+  int getAnioDeLanzamiento();
+
+  void setTitulo(string nuevoTitulo);
+  void setGenero(string nuevoGenero);
+  void setAnioDeLanzamiento(int nuevoAnio);
+};
+
+#endif // __VIDEOJUEGO_H__
